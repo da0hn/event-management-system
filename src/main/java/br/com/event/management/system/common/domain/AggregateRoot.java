a456a8da854a069739e32892097a32b84a9f7fea
Gabriel Honda <gabriel.honda@totemti.com.br>
2023-09-10 11:44:39 -0400

    refactor: move generic `id` to `Entity` abstraction

--- src/main/java/br/com/event/management/system/common/domain/AggregateRoot.java
+++ src/main/java/br/com/event/management/system/common/domain/AggregateRoot.java
@@ -1,27 +1,9 @@
 package br.com.event.management.system.common.domain;
 
-import lombok.Getter;
+public abstract class AggregateRoot<ID extends EntityId<?>> extends Entity<ID> {
 
-@Getter
-public abstract class AggregateRoot<ID extends EntityId<?>> extends Entity {
-
-  private final ID id;
-
-  protected AggregateRoot(final ID id) { this.id = id; }
-
-  @Override
-  public int hashCode() {
-    return this.id.hashCode();
-  }
-
-  @Override
-  public boolean equals(final Object o) {
-    if (this == o) return true;
-    if (o == null || this.getClass() != o.getClass()) return false;
-
-    final AggregateRoot<?> that = (AggregateRoot<?>) o;
-
-    return this.id.equals(that.id);
+  protected AggregateRoot(final ID id) {
+    super(id);
   }
 
 }
--- src/main/java/br/com/event/management/system/common/domain/Entity.java
+++ src/main/java/br/com/event/management/system/common/domain/Entity.java
@@ -1,6 +1,18 @@
 package br.com.event.management.system.common.domain;
 
-public abstract class Entity {
+import lombok.Getter;
+
+@Getter
+public abstract class Entity<ID extends EntityId<?>> {
+
+  private final ID id;
+
+  protected Entity(final ID id) { this.id = id; }
+
+  @Override
+  public int hashCode() {
+    return this.id.hashCode();
+  }
 
   @Override
   public abstract String toString();
