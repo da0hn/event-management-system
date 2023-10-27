464da907b3eb1ca4f9836fad1f7a920c997a3fb9
Gabriel Honda <gabriel.honda@totemti.com.br>
2023-10-26 21:20:03 -0400

    chore: add `email-service`

---
+++ email-service/src/main/kotlin/br/com/event/management/system/email/service/EmailServiceApplication.kt
@@ -0,0 +1,11 @@
+package br.com.event.management.system.email.service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EmailServiceApplication
+
+fun main(args: Array<String>) {
+  runApplication<EmailServiceApplication>(*args)
+}
---
+++ email-service/src/test/kotlin/br/com/event/management/system/email/service/EmailServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.event.management.system.email.service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
