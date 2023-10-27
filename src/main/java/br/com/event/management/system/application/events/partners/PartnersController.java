4b7047f2521b374d450eec692ebacbf141488700
Gabriel Honda <gabriel.honda@totemti.com.br>
2023-10-26 21:07:41 -0400

    feat: add endpoint to update partner

--- src/main/java/br/com/event/management/system/application/events/partners/PartnersController.java
+++ src/main/java/br/com/event/management/system/application/events/partners/PartnersController.java
@@ -1,17 +1,21 @@
 package br.com.event.management.system.application.events.partners;
 
 import br.com.event.management.system.core.events.application.dto.RegisterPartnerInput;
+import br.com.event.management.system.core.events.application.dto.UpdatePartnerInput;
 import br.com.event.management.system.core.events.application.service.PartnerService;
 import br.com.event.management.system.core.events.domain.entities.Partner;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @AllArgsConstructor
@@ -29,6 +33,12 @@ public class PartnersController {
   @PostMapping
   public ResponseEntity<Partner> create(@RequestBody final RegisterPartnerInput input) {
     final var response = this.partnerService.register(input);
+    return ResponseEntity.ok(response);
+  }
+
+  @PutMapping("/{partnerId}")
+  public ResponseEntity<Partner> update(@PathVariable final UUID partnerId, @RequestBody final UpdatePartnerInput input) {
+    final var response = this.partnerService.update(partnerId, input);
     return ResponseEntity.ok(response);
   }
 
